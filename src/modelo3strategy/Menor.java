5632cc32c6c4a82c8537e6ebe9fec25d68ae16b3
programacion <>
2021-05-06 09:47:25 +0200

    recuperando fallo anterior

--- src/modelo3strategy/Menor.java
+++ src/modelo3strategy/Menor.java
@@ -3,7 +3,6 @@ package modelo3strategy;
 public class Menor implements Comportamiento{
 	public float factorDesarrollo;
 
-	// Esto solo los menores
 	@Override
 	public float alimentar(int sueldo,float esperanzaVida) {
 		// TODO
